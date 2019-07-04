a5fca27628124e15da07f0feca4283efd17c5aac
Charles Bentley <cbentley@mail.ru>
2019-07-04 16:12:55 +0200

    initial commit

---
+++ src/pasa/cbentley/core/src8/ctx/C8Ctx.java
@@ -0,0 +1,14 @@
+package pasa.cbentley.core.src8.ctx;
+
+import pasa.cbentley.core.src4.ctx.ACtx;
+import pasa.cbentley.core.src4.ctx.UCtx;
+
+public class C8Ctx extends ACtx {
+
+   
+   public C8Ctx(UCtx uc) {
+      super(uc);
+   }
+
+
+}
