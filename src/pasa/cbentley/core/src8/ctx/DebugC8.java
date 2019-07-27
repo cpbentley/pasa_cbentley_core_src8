2e959ee7ec55ad3408009e3d729d3d7668d9844f
Charles Bentley <cbentley@mail.ru>
2019-07-27 15:02:21 +0200

    new class

---
+++ src/pasa/cbentley/core/src8/ctx/DebugC8.java
@@ -0,0 +1,19 @@
+package pasa.cbentley.core.src8.ctx;
+
+import pasa.cbentley.core.src4.logging.Dctx;
+
+public class DebugC8 {
+
+   public void nlThread(Dctx dcx, String title, Thread t) {
+      Dctx dc = dcx.nLevel(0);
+      if (t == null) {
+         dc.append(title + " is null");
+      } else {
+         dc.append(title);
+         dc.appendVarWithSpace("Name", t.getName());
+         dc.appendVarWithSpace("Priority", t.getPriority());
+         dc.appendVarWithSpace("ID", t.getId());
+
+      }
+   }
+}
